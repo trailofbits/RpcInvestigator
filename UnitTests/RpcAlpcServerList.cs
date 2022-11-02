e79affaebb68d1b76943a72238680db72086cd51
Aaron LeMasters <aaron.lemasters@trailofbits.com>
2022-11-02 13:22:31 -0400

    Initial UnitTest framework commit.

---
+++ UnitTests/RpcAlpcServerList.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RpcInvestigator;
+using RpcInvestigator.TabPages;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RpcAlpcServerListTests
+    {
+        [TestMethod]
+        public void AtLeastOneALPC()
+        {
+            var env = new Environment();
+            env.Initialize();
+            var list = new RpcAlpcServerList(env.m_TabManager);
+            list.Build();
+            Assert.IsTrue(list.GetCount() > 0);
+        }
+    }
+}
